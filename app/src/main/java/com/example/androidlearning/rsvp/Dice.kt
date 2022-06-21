48e7ce7d69d6766d52b064c504f4a6dd31a00633
liver <liver121888@gmail.com>
2022-06-21 16:22:52 +0800

    Finish RSVP Use Room for data persistence.

--- app/src/main/java/com/example/androidlearning/rsvp/Dice.kt
+++
@@ -1,9 +0,0 @@
-package com.example.androidlearning.rsvp
-
-class Dice (private val numSides: Int){
-    var sides = 6
-
-    fun roll(): Int {
-        return (1..numSides).random()
-    }
-}
--- app/src/main/java/com/example/androidlearning/rsvp/marsphotos/network/MarsPhoto.kt
+++
@@ -1,9 +0,0 @@
-package com.example.androidlearning.rsvp.marsphotos.network
-
-import com.squareup.moshi.Json
-
-data class MarsPhoto(
-    val id: String,
-    //    val img_src: String
-    @Json(name = "img_src") val imgSrcUrl: String
-)
